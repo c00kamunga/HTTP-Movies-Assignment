f88e310481b054105544ad4f71722ba18bed75d7
c00kamunga <tsothalanti2@yahoo.com>
2020-05-12 16:16:10 -0700

    exported EditMovie, imported EditMovie into App.js, created a Route with the path provided by readMe and inserted EditMovie inside

---
+++ client/src/Movies/EditMovie.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const EditMovie = () => {
+const [edit, setEdit] = useState({title: "", directors: "", metascore: "", stars: ""})
+}
+
+
+
+
+
+
+
+
+export default EditMovie;
