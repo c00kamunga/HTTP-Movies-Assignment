27a4fcddcf86457dbf3f3ef565d5da690bb40763
c00kamunga <tsothalanti2@yahoo.com>
2020-05-12 16:54:43 -0700

    set the state of for the form to include empty strings, created changeHandler function for form, passed in changeHandler to onChange attribute, event target name attribute from input field

--- client/src/Movies/EditMovie.js
+++ client/src/Movies/EditMovie.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+const { title, director, metascore, stars } = props.movies;
 
 const EditMovie = () => {
-const [edit, setEdit] = useState({title: "", directors: "", metascore: "", stars: ""})
-}
+  const [edit, setEdit] = useState({
+    title: "",
+    director: "",
+    metascore: "",
+    stars: "",
+  });
 
+  const changeHandler = (e) => {
+    setMovie({ ...edit, [e.target.name]: e.target.value });
+  };
 
-
-
-
-
-
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input type="text" name="title" value={edit.title} onChange={changeHandler} />
+        <input type="text" name="director" value={edit.director} onChange={changeHandler}/>
+        <input type="text" name="metascore" value={edit.metascore} onChange={changeHandler}/>
+        <input type="text" name="stars" value={edit.stars} onChange={changeHandler}/>
+        <button type="submit">Edit</button>
+      </form>
+    </div>
+  );
+};
 
 export default EditMovie;
